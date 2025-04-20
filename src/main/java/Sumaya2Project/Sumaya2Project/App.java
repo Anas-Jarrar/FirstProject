14bbdf9545d28ce15cc256877dd7a9b7b2285274
Anas-Jarrar <anasjarrar944@gmail.com>
2025-04-21 00:49:09 +0300

    update

---
+++ src/main/java/Sumaya2Project/Sumaya2Project/App.java
@@ -0,0 +1,11 @@
+package Sumaya2Project.Sumaya2Project;
+
+import org.testng.annotations.Test;
+
+public class App {
+	
+	
+   @Test
+   public void myTest() {}
+   
+}
---
+++ src/test/java/Sumaya2Project/Sumaya2Project/AppTest.java
@@ -0,0 +1,13 @@
+package Sumaya2Project.Sumaya2Project;
+
+
+public class AppTest {
+
+	
+	
+	
+	public void Mytest() {
+		
+		
+	}
+}
